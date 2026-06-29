ecc03232b49a1084ee790426f4a4bfb48f8560a5
Janne Saranpää <janne@jssa.fi>
2026-06-29 12:28:13 +0300

    feat(backend): add logging
    Use tracing and tracing_subscriber for logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,21 @@
 use axum::{Router, http::StatusCode, routing::get};
+use tracing::info;
+
+use tracing_subscriber::prelude::*;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new().route("/health", get(handle_health));
 
     let addr = "127.0.0.1:8080";
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    info!("Apu server running on http://{}", addr);
 
     axum::serve(listener, app).await.unwrap();
 }
