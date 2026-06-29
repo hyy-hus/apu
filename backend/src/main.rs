4c6132bb771fb768a144eb73f9b563c826077381
Janne Saranpää <janne@jssa.fi>
2026-06-29 11:48:45 +0300

    feat(backend): create an axum backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, http::StatusCode, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health", get(handle_health));
+
+    let addr = "127.0.0.1:8080";
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_health() -> StatusCode {
+    StatusCode::OK
 }
